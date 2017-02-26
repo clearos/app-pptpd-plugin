1287b31eaa974e74f3aee541c64dc3273ae080d6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:09:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'Politiki Server PPTP jipprovdi kontroll ta &#39;aċċess għall-app server PPTP.';
+$lang['pptpd_plugin_app_name'] = 'Politiki Server PPTP';
+$lang['pptpd_plugin_pptp_server_user'] = 'Utent PPTP Server';
