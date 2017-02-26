0dc1a73375c7ee85604882d1ad4434771defccdc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:09:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'पिपिटिपि सर्भर नीतिहरू पनि पिपिटिपि सर्भर अनुप्रयोगको लागि पहुँच नियन्त्रण प्रदान।';
+$lang['pptpd_plugin_app_name'] = 'पिपिटिपि सर्भर नीतिहरू';
+$lang['pptpd_plugin_pptp_server_user'] = 'पिपिटिपि सर्भर प्रयोगकर्ता';
