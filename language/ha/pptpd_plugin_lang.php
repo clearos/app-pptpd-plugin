f92d27d3f473fd3b4ed87ed804ac5be125786919
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:05:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP Server Asiri samar da damar iko ga PPTP uwar garken app.';
+$lang['pptpd_plugin_app_name'] = 'PPTP Server Asiri';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server User';
