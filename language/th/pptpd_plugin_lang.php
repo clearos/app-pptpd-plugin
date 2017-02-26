f797424f142bc9a47d047129cb9f633e7826fe62
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:12:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'นโยบายเซิร์ฟเวอร์ PPTP ให้การควบคุมการเข้าถึงสำหรับแอปพลิเคเซิร์ฟเวอร์ PPTP';
+$lang['pptpd_plugin_app_name'] = 'นโยบายเซิร์ฟเวอร์ PPTP';
+$lang['pptpd_plugin_pptp_server_user'] = 'ผู้ใช้เซิร์ฟเวอร์ PPTP';
