21b7afa2db22165d93967dc398c05e434d3a3523
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:08:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP сервер Бодлого PPTP сервер програм нь хандалтын хяналтыг олгодог.';
+$lang['pptpd_plugin_app_name'] = 'PPTP сервер бодлого';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP сервер хэрэглэгч';
