a2dde531718b0a75592b24b1bb4b2e15ed2aae30
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:13:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'פּפּטפּ סערווירער פּאַלאַסיז צושטעלן צוטריט קאָנטראָל פֿאַר די פּפּטפּ סערווירער אַפּ.';
+$lang['pptpd_plugin_app_name'] = 'פּפּטפּ סערווירער פּאָליסיעס';
+$lang['pptpd_plugin_pptp_server_user'] = 'פּפּטפּ סערווירער באַניצער';
