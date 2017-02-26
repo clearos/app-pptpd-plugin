caf5904368c3ce141d9305c3ab8bb4cae82ce5ca
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:01:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP սերվեր քաղաքականությունը տրամադրել մուտքի վերահսկման համար PPTP սերվերի հավելվածի:';
+$lang['pptpd_plugin_app_name'] = 'PPTP սերվեր քաղաքականություն';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server Օգտվող';
