d84dc3b37e7bb6c9491871007128101c02f6ca54
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:03:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP服务器策略提供PPTP服务器应用程序的访问控制。';
+$lang['pptpd_plugin_app_name'] = 'PPTP服务器策略';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP服务器用户';
