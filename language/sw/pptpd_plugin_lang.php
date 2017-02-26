1704806b60e6fb4abb8fd027f3d29c4729863f5e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:11:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP Sera Server kutoa upatikanaji wa kudhibiti kwa programu PPTP server.';
+$lang['pptpd_plugin_app_name'] = 'Sera PPTP Server';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server mtumiaji';
