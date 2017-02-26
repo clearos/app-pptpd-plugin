9a9acd41c7900d26815cc80fd6a05e21b774845d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:04:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP Servilo Politikoj havigi aliron kontrolo por PPTP servilo app.';
+$lang['pptpd_plugin_app_name'] = 'PPTP Servilo Politikoj';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Servilo Uzanto';
