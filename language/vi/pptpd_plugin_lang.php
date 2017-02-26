1d7ad0d8e11e42ddb99d2fff87ec477a94782a05
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:13:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'Máy chủ Chính sách PPTP cung cấp kiểm soát truy cập cho các ứng dụng máy chủ PPTP.';
+$lang['pptpd_plugin_app_name'] = 'Chính sách PPTP server';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP server tài';
