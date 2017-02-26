24b65e8ed518c32ce731e83c5a39786b2c0584e5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:12:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'Сиёсати PPTP Server таъмин идоракунии дастрасӣ, барои барнома сервер PPTP.';
+$lang['pptpd_plugin_app_name'] = 'Сиёсати Server PPTP';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server Корбар';
