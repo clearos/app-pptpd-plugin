2e8c7b1bda1eb4d363c7d00a29f01288d8024930
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:11:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'Siyaasadaha Server PPTP bixiyaan helitaanka gacanta for app server PPTP ah.';
+$lang['pptpd_plugin_app_name'] = 'PPTP Siyaasadaha Server';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server User';
