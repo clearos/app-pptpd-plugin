cb11e8abea176dee2dee64be07367ce36168d69b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:02:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'পিপিটিপি সার্ভার নীতিসমূহ পিপিটিপি সার্ভার অ্যাপ্লিকেশন জন্য প্রবেশাধিকার নিয়ন্ত্রণ প্রদান.';
+$lang['pptpd_plugin_app_name'] = 'পিপিটিপি সার্ভার নীতিসমূহ';
+$lang['pptpd_plugin_pptp_server_user'] = 'পিপিটিপি সার্ভার ব্যবহারকারী';
