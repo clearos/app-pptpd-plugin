a4b72c3f8a7adc5712c792c4fe3e97f73929d3a7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:01:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'توفر سياسات خادم PPTP التحكم في الوصول للتطبيقات الخادم PPTP.';
+$lang['pptpd_plugin_app_name'] = 'سياسات خادم PPTP';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP خادم العضو';
