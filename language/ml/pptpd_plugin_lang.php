f04467bde75f3c11143bf9b8f46042e59a09bdfc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:08:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP സെർവർ നയങ്ങള് PPTP സെർവർ അപ്ലിക്കേഷൻ ആക്സസ്സ് നിയന്ത്രണം നൽകുന്നു.';
+$lang['pptpd_plugin_app_name'] = 'PPTP സെർവർ നയങ്ങള്';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP സെർവർ ഉപയോക്തൃ';
