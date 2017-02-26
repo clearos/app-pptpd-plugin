54e7b9a8f9c2d14df14722fc685fad89f8ea513f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:05:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'Ptp Règleman sèvè bay aksè kontwòl pou app a sèvè ptp.';
+$lang['pptpd_plugin_app_name'] = 'Ptp sèvè Règleman';
+$lang['pptpd_plugin_pptp_server_user'] = 'Ptp sèvè Itilizatè';
