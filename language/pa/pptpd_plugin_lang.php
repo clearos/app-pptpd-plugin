652b2282ab320de72ee28506f5bd9a06b8e6e8bd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:09:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP ਸਰਵਰ ਡਰਾਇਰ PPTP ਸਰਵਰ ਐਪ ਲਈ ਪਹੁੰਚ ਕੰਟਰੋਲ ਮੁਹੱਈਆ ਕਰਦਾ ਹੈ.';
+$lang['pptpd_plugin_app_name'] = 'PPTP ਸਰਵਰ ਡਰਾਇਰ';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP ਸਰਵਰ ਲਈ ਯੂਜ਼ਰ';
