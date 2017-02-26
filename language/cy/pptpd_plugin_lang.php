fbb0b493826d80e33bf20dc6f0f4b40a28442971
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:13:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'Polisïau Gweinydd PPTP darparu rheolaeth mynediad ar gyfer y app gweinydd PPTP.';
+$lang['pptpd_plugin_app_name'] = 'Polisïau Gweinydd PPTP';
+$lang['pptpd_plugin_pptp_server_user'] = 'Defnyddiwr PPTP Gweinydd';
