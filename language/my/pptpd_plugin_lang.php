5c1038ba03080b004c4be1b06c3ebcd0d7454044
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:02:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP ကို ​​Server ကိုမူဝါဒများအ PPTP server ကို app ကိုများအတွက်ဝင်ရောက်ခွင့်ကိုထိန်းချုပ်ပေး။';
+$lang['pptpd_plugin_app_name'] = 'PPTP ကို ​​Server ကိုမူဝါဒများ';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP ကို ​​Server ကိုအသုံးပြုသူတို့၏';
