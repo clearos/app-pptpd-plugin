e083cb1e984b2a2809e4d26faf7bc123495d9ed0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:06:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTPサーバーポリシーは、PPTPサーバーアプリケーションのアクセス制御を提供します。';
+$lang['pptpd_plugin_app_name'] = 'PPTPサーバーポリシー';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTPサーバーのユーザー';
