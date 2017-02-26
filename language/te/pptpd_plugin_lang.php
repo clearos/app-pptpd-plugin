3dc63e0b3a80e4e557fbf086009434c16ef62060
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:12:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP సర్వర్ విధానాలు PPTP సర్వర్ అనువర్తనం ప్రాప్యత నియంత్రణను అందిస్తాయి.';
+$lang['pptpd_plugin_app_name'] = 'PPTP సర్వర్ విధానాలు';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP సర్వర్ వాడుకరి';
