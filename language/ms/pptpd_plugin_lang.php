2c1782072877792040e210fbebb046dafb8333e2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:09:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP Polisi Server menyediakan kawalan akses untuk aplikasi pelayan PPTP.';
+$lang['pptpd_plugin_app_name'] = 'PPTP Polisi Server';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server Pengguna';
