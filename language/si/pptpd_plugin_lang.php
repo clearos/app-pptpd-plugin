b81047b17cbd3f26cbd9f73bff4baacf39d42767
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:10:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP සේවාදායකය ප්රතිපත්ති මෙම PPTP සේවාදායකය යෙදුම සඳහා ප්රවේශ පාලක ලබා දෙයි.';
+$lang['pptpd_plugin_app_name'] = 'PPTP සේවාදායකය ප්රතිපත්ති';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP සේවාදායකය පරිශීලක';
