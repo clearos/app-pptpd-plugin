308b24f6786a7ade1428803a4e02b39f252b0e88
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:03:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP Ndondomeko Server imapatsa kulamulira kwa app PPTP Seva.';
+$lang['pptpd_plugin_app_name'] = 'Ndondomeko PPTP Server';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server Wosuta';
