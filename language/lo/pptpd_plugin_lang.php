7e3c98fd2a35297178036c3dc9423861be7e7bb1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:07:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'ນະໂຍບາຍ Server PPTP ໃຫ້ການຄວບຄຸມການເຂົ້າເຖິງສໍາລັບ app server PPTP ໄດ້.';
+$lang['pptpd_plugin_app_name'] = 'ນະໂຍບາຍ Server PPTP';
+$lang['pptpd_plugin_pptp_server_user'] = 'User PPTP Server';
