2ae69fe07875740e6cd2100c5ab94405b8d683c5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:06:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP Server Reglur veita aðgang stjórna fyrir PPTP miðlara app.';
+$lang['pptpd_plugin_app_name'] = 'PPTP Server Reglur';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server User';
