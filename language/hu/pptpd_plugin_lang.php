03ae92c8230b70ccdd3ed32df592a421397bb274
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:05:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP szerver házirendje eléréskorlátozást a PPTP szerver alkalmazás.';
+$lang['pptpd_plugin_app_name'] = 'PPTP Server házirendje';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server felhasználói';
