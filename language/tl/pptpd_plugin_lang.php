36cc0dc23a9e6bdbd5831ba4afe4ccf5d8a18a64
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:12:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP Patakaran Server magbigay ng access control para sa PPTP server app.';
+$lang['pptpd_plugin_app_name'] = 'PPTP Patakaran Server';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server User';
