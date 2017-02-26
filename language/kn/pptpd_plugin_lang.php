cafae1b79c4dad2c5ca97fa6da7d5c21317d65cf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:07:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP ಸರ್ವರ್ ನೀತಿಗಳು PPTP ಸರ್ವರ್ ಅಪ್ಲಿಕೇಶನ್ ಆಕ್ಸೆಸ್ ಕಂಟ್ರೋಲ್ ನೀಡಲಾಗುತ್ತದೆ.';
+$lang['pptpd_plugin_app_name'] = 'PPTP ಸರ್ವರ್ ನೀತಿಗಳು';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP ಸರ್ವರ್ ಬಳಕೆದಾರ';
