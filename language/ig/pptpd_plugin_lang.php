0e675967de65244ace9c55a1a3cdc607aa8cb542
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:06:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP Server Atumatu-enye ohere akara maka PPTP nkesa na-eji ngwa.';
+$lang['pptpd_plugin_app_name'] = 'PPTP Server Atumatu';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server User';
