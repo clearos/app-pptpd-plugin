30a19b6fea7f242463d93c0f1d79a84ff9666f4f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:12:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP சர்வர் கொள்கைகள் PPTP சர்வர் பயன்பாட்டை கட்டுப்படுத்த உதவும்.';
+$lang['pptpd_plugin_app_name'] = 'PPTP சர்வர் கொள்கைகள்';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP சர்வர் பயனர்';
