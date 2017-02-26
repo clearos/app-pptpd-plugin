6f94da15ab3e0f54447e741094495a6e9617146d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:01:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP Server Beleid voorsien toegangsbeheer vir die PPTP-bediener app.';
+$lang['pptpd_plugin_app_name'] = 'Server Beleid PPTP';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server User';
