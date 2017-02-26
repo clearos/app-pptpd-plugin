dabbae83cd925ec998a2367688f5870648c8372c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:14:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP Izinqubomgomo Server ukunikeza control ukufinyelela lokusebenza PPTP iseva.';
+$lang['pptpd_plugin_app_name'] = 'PPTP Izinqubomgomo Server';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server Umsebenzisi';
