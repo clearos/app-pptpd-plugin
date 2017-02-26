cbd9b975fa5ff422d33b009ff2824309e02b1c02
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:01:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP Politikat Server sigurojë kontrollin e qasjes për app server PPTP.';
+$lang['pptpd_plugin_app_name'] = 'PPTP Politikat Server';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server User';
