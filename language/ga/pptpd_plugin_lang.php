15ba19887eed13cef1f860734f3cc83a12221b1d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:06:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'a chur ar fáil Polasaithe Freastalaí PPTP rialaithe rochtana don app fhreastalaí PPTP.';
+$lang['pptpd_plugin_app_name'] = 'Polasaithe Freastalaí PPTP';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Freastalaí Úsáideoir';
