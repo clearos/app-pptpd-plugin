5bf570b3940c0be5287acceac406e5435f09dcd5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:07:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'Servo PPTP Policies imperium providere accessum ad PPTP server App.';
+$lang['pptpd_plugin_app_name'] = 'Servo PPTP Politiae';
+$lang['pptpd_plugin_pptp_server_user'] = 'User PPTP Server';
