0d25c66e4c46b8c000f787a751d330d3c90628aa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:13:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP Server imulo pese wiwọle iṣakoso fun awọn PPTP server app.';
+$lang['pptpd_plugin_app_name'] = 'PPTP Server imulo';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server User';
