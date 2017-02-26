25be93b82441c2b5e79fdbfc6573ad54b0e777d2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:08:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'whakarato kaupapa here Tūmau PPTP mana uru mo te taupānga tūmau PPTP.';
+$lang['pptpd_plugin_app_name'] = 'Kaupapa here Tūmau PPTP';
+$lang['pptpd_plugin_pptp_server_user'] = 'Kaiwhakamahi PPTP Tūmau';
