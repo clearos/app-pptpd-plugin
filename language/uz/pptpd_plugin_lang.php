29a86f1fa446ba10f68c3d598fd8013494aa9ddf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:13:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP Server siyosati PPTP server ilova uchun erkin foydalanish nazorat beradi.';
+$lang['pptpd_plugin_app_name'] = 'PPTP Server siyosati';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server Foydalanuvchi';
