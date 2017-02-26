b30806a23c54149e29de362ce459b78d823febde
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:04:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP Server käytännöt tarjoavat kulunvalvonta varten PPTP-palvelimen sovellus.';
+$lang['pptpd_plugin_app_name'] = 'PPTP Server säännöt';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server User';
