ff8a68f0d31cb650a0f635cbb7ac7751d08b2598
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:11:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP Kawijakan Server nyadiakeun kadali aksés pikeun app PPTP server.';
+$lang['pptpd_plugin_app_name'] = 'PPTP Kawijakan Server';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server Pamaké';
