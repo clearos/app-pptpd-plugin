564f09be7bca2966b5ef47a93def5a6f9995690d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:04:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'PPTP Πολιτικές διακομιστή παρέχει έλεγχο πρόσβασης για την εφαρμογή του διακομιστή PPTP.';
+$lang['pptpd_plugin_app_name'] = 'Πολιτικές διακομιστή PPTP';
+$lang['pptpd_plugin_pptp_server_user'] = 'PPTP Server χρήστη';
