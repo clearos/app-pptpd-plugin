7eefb7ed03a643b1792bdf4d45fee7bab6c47a81
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:07:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/pptpd_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['pptpd_plugin_app_description'] = 'គោលនយោបាយម៉ាស៊ីនបម្រើ PPTP ផ្ដល់នូវការត្រួតពិនិត្យការចូលដំណើរការសម្រាប់កម្មវិធីរបស់ម៉ាស៊ីនបម្រើ PPTP ។';
+$lang['pptpd_plugin_app_name'] = 'គោលនយោបាយម៉ាស៊ីនបម្រើ PPTP';
+$lang['pptpd_plugin_pptp_server_user'] = 'អ្នកប្រើប្រាស់ PPTP ម៉ាស៊ីនបម្រើ';
